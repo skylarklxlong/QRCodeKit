8706ed788f49622912368a2ea4910d6732bf806f
Skylark <li_xuelong@126.com>
2018-03-08 19:57:11 +0800

    first commit

---
+++ app/src/main/java/online/himakeit/qrcodekit/MainActivity.java
@@ -0,0 +1,19 @@
+package online.himakeit.qrcodekit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @author：LiXueLong
+ * @date:2018/3/8-19:52
+ * @mail1：skylarklxlong@outlook.com
+ * @mail2：li_xuelong@126.com
+ * @des：MainActivity
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
